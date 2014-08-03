b80f5ee8ff4302b052986cb6cd13f4f540bb9d68
zyunx <zyunone@163.com>
2014-08-03 18:52:54 -0400

    Add some docs and begin writing code for reading .bmp format file. Reading .png file support is delayed.

---
+++ Makefile
@@ -0,0 +1,1 @@
+readpng: readpng.c
---
+++ readbmp.c
@@ -0,0 +1,128 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <assert.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#define PNG_0	0x89
+#define PNG_1	0x50
+#define PNG_2	0x4E
+#define PNG_3	0x47
+#define PNG_4	0x0D
+#define PNG_5	0x0A
+#define PNG_6	0x1A
+#define PNG_7	0x0A
+
+#define FILENAME "an.bmp"
+
+#define hton32(x) \
+	((((x) & 0xFF000000) >> 24) | \
+	 (((x) & 0x00FF0000) >>  8) | \
+	 (((x) & 0x0000FF00) <<  8) | \
+	 (((x) & 0x000000FF) << 24))
+
+#define ntoh32 hton32
+
+struct BITMAPFILEHEADER
+{
+	uint8_t BM[2];
+	uint32_t size;
+	uint16_t reserved1;
+	uint16_t reserved2;
+	uint32_t pixel_array_offset;
+};
+
+static char image_data[10000000];
+static int image_data_size = 0;
+
+int main()
+{
+	unsigned char *addr;
+	unsigned char *chunks;
+	struct chunk_header *chdr;
+	struct chunkdata_IHDR *ihdr;
+	int fd;
+	struct stat sb;
+
+	fd = open(FILENAME, O_RDONLY);
+	assert(fd != -1);
+
+	assert(fstat(fd, &sb) != -1);
+
+	addr = mmap(NULL, sb.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+	assert(addr != MAP_FAILED);
+
+	//printf("%x\n", addr[0]);
+	if (addr[0] == 'B' && addr[1] == 'M')
+	{
+		printf("BMP format file\n");
+		/*
+		chunks = addr + 8;
+
+		printf("chuncks:\n");
+		do
+		{
+			chdr = (struct chunk_header*) chunks;
+			chunks += 8 + hton32(chdr->len) + 4;
+			printf("%c%c%c%c %d\n", chdr->type[0],
+					chdr->type[1],
+					chdr->type[2],
+					chdr->type[3],
+					hton32(chdr->len));
+
+			if (chdr->type[0] == 'I' &&
+					chdr->type[1] == 'H' &&
+					chdr->type[2] == 'D' &&
+					chdr->type[3] == 'R')
+			{
+				ihdr = (struct chunkdata_IHDR*)(chdr + 1);
+				printf("Width: %u Height: %u " \
+						"Bit depth: %u Color type: %u\n" \
+						"Compression: %u " \
+						"Filter: %u " \
+						"Interlace: %u\n",
+						ntoh32(ihdr->width),
+						ntoh32(ihdr->height),
+						ihdr->bit_depth,
+						ihdr->color_type,
+						ihdr->compression_method,
+						ihdr->filter_method,
+						ihdr->interlace_method);
+			} else if (chdr->type[0] == 'I' &&
+					chdr->type[1] == 'D' &&
+					chdr->type[2] == 'A' &&
+					chdr->type[3] == 'T')
+			{
+				printf("m1\n");
+				memcpy(&(image_data[image_data_size]),
+						chdr + 1,
+						chdr->len);
+printf("m\n");
+				if (image_data_size == 0)
+				{
+					printf("Zlib:\n");
+					printf("Compression method: %d " \
+							"Additional flags: %d\n",
+							image_data[image_data_size],
+							image_data[image_data_size+1]);
+				}
+				
+				image_data_size += chdr->len;	
+			}
+
+		} while(!(chdr->type[0] == 'I' &&
+					chdr->type[1] == 'E' &&
+					chdr->type[2] == 'N' &&
+					chdr->type[3] == 'D'));
+					*/
+	}
+
+	assert(munmap(addr, sb.st_size) != -1);
+
+	assert(close(fd) != -1);
+
+	return EXIT_SUCCESS;
+}
